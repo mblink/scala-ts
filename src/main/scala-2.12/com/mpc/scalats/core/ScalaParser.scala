a435beb693d731f12723cca4c51246d1844c5ecb
Justin Leider <justin@leiders.org>
2019-12-18 12:09:48 -0500

    Move excludetypes check to parsing step

--- src/main/scala-2.12/com/mpc/scalats/core/ScalaParser.scala
+++ src/main/scala-2.12/com/mpc/scalats/core/ScalaParser.scala
@@ -16,36 +16,29 @@ final class ScalaParser(logger: Logger, mirror: Mirror, excludeTypes: List[Type]
   def parseTypes(types: List[Type]): ListSet[TypeDef] =
     parse(types, ListSet[Type](), ListSet.empty[TypeDef])
 
-  // ---
 
-  private def parseType(tpe: Type): Option[TypeDef] = {
-    if (!excludeTypes.contains(tpe)) {
-      tpe match {
-        case _: SingleTypeApi =>
-          parseObject(tpe)
+  private def parseType(tpe: Type): Option[TypeDef] = tpe match {
+    case _: SingleTypeApi =>
+      parseObject(tpe)
 
-        case _ if (tpe.getClass.getName contains "ModuleType" /*Workaround*/) =>
-          parseObject(tpe)
+    case _ if (tpe.getClass.getName contains "ModuleType" /*Workaround*/) =>
+      parseObject(tpe)
 
-        case _ if tpe.typeSymbol.isClass && !tpe.typeSymbol.name.toString.contains("NonEmptyList") => {
-          val classSym = tpe.typeSymbol.asClass
+    case _ if tpe.typeSymbol.isClass && !tpe.typeSymbol.name.toString.contains("NonEmptyList") => {
+      val classSym = tpe.typeSymbol.asClass
 
-          if (classSym.isTrait && classSym.isSealed && tpe.typeParams.isEmpty) {
-            parseSealedUnion(tpe)
-          } else if (isCaseClass(tpe) && !isAnyValChild(tpe)) {
-            parseCaseClass(tpe)
-          } else {
-            Option.empty[TypeDef]
-          }
-        }
-
-        case _ => {
-          logger.warning(s"Unsupported Scala type: $tpe")
-          Option.empty[TypeDef]
-        }
+      if (classSym.isTrait && classSym.isSealed && tpe.typeParams.isEmpty) {
+        parseSealedUnion(tpe)
+      } else if (isCaseClass(tpe) && !isAnyValChild(tpe)) {
+        parseCaseClass(tpe)
+      } else {
+        Option.empty[TypeDef]
       }
-    } else{
-      None
+    }
+
+    case _ => {
+      logger.warning(s"Unsupported Scala type: $tpe")
+      Option.empty[TypeDef]
     }
   }
 
@@ -150,8 +143,7 @@ final class ScalaParser(logger: Logger, mirror: Mirror, excludeTypes: List[Type]
   @annotation.tailrec
   private def parse(types: List[Type], examined: ListSet[Type], parsed: ListSet[TypeDef]): ListSet[TypeDef] = types match {
     case scalaType :: tail => {
-      if (!examined.contains(scalaType) &&
-        !scalaType.typeSymbol.isParameter) {
+      if (!excludeTypes.contains(scalaType) && !examined.contains(scalaType) && !scalaType.typeSymbol.isParameter) {
 
         val relevantMemberSymbols = scalaType.members.collect {
           case m: MethodSymbol if isValidMethod(m) => m
